70d9461ef2dee9a9cc1306bbb53cee6cbc779818
Carlos Eduardo Cordón <carlos.edu.cordon@gmail.com>
2020-02-21 17:09:41 -0600

    Revert "fix: folder structure fix"

--- amdvatapp/src/app/mocks/mock-demo.ts
+++
@@ -1,8 +0,0 @@
-import {Demo} from '../models/demo';
-
-export const Demos: Array<Demo> = [
-    {id: '1', name: 'uno'},
-    {id: '2', name: 'dos'},
-    {id: '3', name: 'tres'},
-    {id: '4', name: 'cuatro'},
-];
