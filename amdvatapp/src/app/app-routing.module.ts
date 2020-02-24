99779c8f69445abbda072a3476a6c24f7b3cd274
Fernando Flores <nbafernandoflores8@gmail.com>
2020-02-23 21:45:21 -0600

    fix: UI fix 2

--- amdvatapp/src/app/app-routing.module.ts
+++ amdvatapp/src/app/app-routing.module.ts
@@ -1,27 +1,26 @@
-import {NgModule} from '@angular/core';
-import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
-import {LogInComponent} from './components/log-in/log-in.component';
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LogInComponent } from './components/log-in/log-in.component';
 
 const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'folder/Inbox',
-        pathMatch: 'full'
-    },
-    {
-        path: 'folder/:id',
-        loadChildren: () => import('./folder/folder.module').then(m => m.FolderPageModule)
-    }, {
-        path: 'login',
-        component: LogInComponent
-    }
+  {
+    path: '',
+    redirectTo: 'folder/Inbox',
+    pathMatch: 'full'
+  },
+  {
+    path: 'folder/:id',
+    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },{
+    path: 'login',
+    component: LogInComponent
+  }
 ];
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
-    ],
-    exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
