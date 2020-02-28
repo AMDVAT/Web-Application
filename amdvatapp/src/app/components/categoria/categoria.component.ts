3b798a663cdef9fc802f22d2625c2138df3a7a89
Fernando Flores <nbafernandoflores8@gmail.com>
2020-02-28 11:32:45 -0600

    ci: imports sintax fix

--- amdvatapp/src/app/components/categoria/categoria.component.ts
+++ amdvatapp/src/app/components/categoria/categoria.component.ts
@@ -1,10 +1,10 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Router, ActivatedRoute, ParamMap} from '@angular/router';
 import {switchMap} from 'rxjs/operators';
-import {CategoriaService} from "../../services/categoria/categoria.service";
-import {Categoria} from "../../models/Categoria";
-import {Producto} from "../../models/Producto";
-import {ProductoService} from "../../services/producto/producto.service";
+import {CategoriaService} from '../../services/categoria/categoria.service';
+import {Categoria} from '../../models/Categoria';
+import {Producto} from '../../models/Producto';
+import {ProductoService} from '../../services/producto/producto.service';
 
 @Component({
     selector: 'app-categoria',
