fd9b11aec7bac3796e741ffef455a66bf9358aa2
Fernando Flores <nbafernandoflores8@gmail.com>
2020-02-28 10:18:05 -0600

    ci: productos mock

---
+++ amdvatapp/src/app/mocks/mock-productos.ts
@@ -0,0 +1,54 @@
+import {Producto} from '../models/Producto';
+
+export const productos: Array<Producto> = [
+    {
+        idProducto: 1,
+        nombre: 'PC',
+        descripcion: '',
+        calificacion: 3,
+        foto: '',
+        precio: 50.50,
+        precioOferta: 20.20,
+        status: 0
+    },
+    {
+        idProducto: 2,
+        nombre: 'Laptop',
+        descripcion: '',
+        calificacion: 4,
+        foto: '',
+        precio: 150.50,
+        precioOferta: 90.20,
+        status: 0
+    },
+    {
+        idProducto: 3,
+        nombre: 'Celular',
+        descripcion: '',
+        calificacion: 2,
+        foto: '',
+        precio: 20.20,
+        precioOferta: 40.20,
+        status: 0
+    },
+    {
+        idProducto: 4,
+        nombre: 'Tablet',
+        descripcion: '',
+        calificacion: 5,
+        foto: '',
+        precio: 160.99,
+        precioOferta: 150.20,
+        status: 0
+    },
+    {
+        idProducto: 5,
+        nombre: 'Smart Watch',
+        descripcion: '',
+        calificacion: 4,
+        foto: '',
+        precio: 150.60,
+        precioOferta: 40.20,
+        status: 0
+    }
+];
