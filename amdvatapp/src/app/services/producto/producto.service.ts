48426fdfef1d4249300ab60c17cf4115464bc884
Fernando Flores <nbafernandoflores8@gmail.com>
2020-02-28 10:21:30 -0600

    ci(getProducto): get producto by id

--- amdvatapp/src/app/services/producto/producto.service.ts
+++ amdvatapp/src/app/services/producto/producto.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Producto} from '../../models/Producto';
+import {productos} from '../../mocks/mock-productos';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
 
-  constructor() { }
+  constructor() {
+  }
+
+  getProducto(id: number): Producto {
+    return productos.find(prod => prod.idProducto === id);
+  }
 }
