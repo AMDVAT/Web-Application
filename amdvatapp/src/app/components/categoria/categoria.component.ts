f984c50a6c3a32f4d1c8991f4ccb9b74a6b594c3
Fernando Flores <nbafernandoflores8@gmail.com>
2020-02-28 10:04:37 -0600

    ci: categoria name display

--- amdvatapp/src/app/components/categoria/categoria.component.ts
+++ amdvatapp/src/app/components/categoria/categoria.component.ts
@@ -1,5 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Categoria} from '../../models/Categoria';
+import {Router, ActivatedRoute, ParamMap} from '@angular/router';
+import {switchMap} from 'rxjs/operators';
+import {CategoriaService} from "../../services/categoria/categoria.service";
+import {Categoria} from "../../models/Categoria";
 
 @Component({
     selector: 'app-categoria',
@@ -7,11 +10,18 @@ import {Categoria} from '../../models/Categoria';
     styleUrls: ['./categoria.component.scss'],
 })
 export class CategoriaComponent implements OnInit {
+    categoria: Categoria;
 
-    constructor() {
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private categoriaService: CategoriaService
+    ) {
     }
 
     ngOnInit() {
+        const id = this.route.snapshot.paramMap.get('id');
+        this.categoria = this.categoriaService.getCategoria(parseInt(id, 10));
     }
 
 }
