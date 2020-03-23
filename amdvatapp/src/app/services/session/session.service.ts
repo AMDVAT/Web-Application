db60e82897f5d03c8f0fe649a116bd5ac4e7b0ca
Fernando Flores <nbafernandoflores8@gmail.com>
2020-03-23 13:31:11 -0600

    ci: setUser function add

--- amdvatapp/src/app/services/session/session.service.ts
+++ amdvatapp/src/app/services/session/session.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {NativeStorage} from '@ionic-native/native-storage/ngx';
+import {LogInService} from '../log-in/log-in.service';
+import {Platform} from '@ionic/angular';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class SessionService {
 
-  constructor() { }
+    constructor(
+        private nativeStorage: NativeStorage,
+        public platform: Platform
+    ) {
+    }
+
+    setUser(user: any): void {
+        if (this.platform.is('android')) {
+            this.nativeStorage.setItem('user', user)
+                .then(
+                    (data) => console.log('Stored first item!', data),
+                    error => console.error('Error storing item', error)
+                );
+        } else {
+            localStorage.setItem('user', JSON.stringify(user));
+        }
+    }
 }
+
