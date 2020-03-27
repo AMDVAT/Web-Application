161ad4c7c51851cbbad4f2b139c2adbbc3db4bd1
Diego Josue Berrios Gutierrez <diegoberrios15@gmail.com>
2020-03-27 00:20:57 -0600

    update gestion-usuario service
    se actualizaron los servicios que se van a consumir

--- amdvatapp/src/app/services/gestion-usuarios/gestion-usuario.service.ts
+++ amdvatapp/src/app/services/gestion-usuarios/gestion-usuario.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {EnviromentService} from '../enviroment/enviroment.service';
+import {User} from '../../models/user'
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +18,20 @@ export class GestionUsuarioService {
     //http://amdvat-be.herokuapp.com
   }
 
+  getOneUser(id: string){
+    return this.http.get(`${this.env.API_URI}usuario/`);
+  }
   
+  saveUser(usuario: User){
+    return this.http.post(`${this.env.API_URI}/usuario/registrer`,usuario);
+  }
 
+  deleteUser(){
+
+  }
+
+  updateUser(id: string, updateUser: User){
+    return this.http.put(`${this.env.API_URI}/usuario/editar/${id}`,updateUser);
+  }
 
 }
