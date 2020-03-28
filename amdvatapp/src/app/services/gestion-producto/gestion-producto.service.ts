1a8ac01b759eb8c62f68d2454fcffdf6303cc13d
Diego Josue Berrios Gutierrez <diegoberrios15@gmail.com>
2020-03-28 16:24:33 -0600

    update list-productos
    se agrego la funcion de mostrar los productos a la pestaña de gestion de productos

--- amdvatapp/src/app/services/gestion-producto/gestion-producto.service.ts
+++ amdvatapp/src/app/services/gestion-producto/gestion-producto.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import {EnviromentService} from '../enviroment/enviroment.service';
+import { HttpClient } from '@angular/common/http';
+import {Producto} from '../../models/Producto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GestionProductoService {
 
-  constructor( ) { }
+  constructor(
+    private http: HttpClient,
+    private env: EnviromentService
+   ) { }
+
+    getListProduct(){
+      return this.http.get(`${this.env.API_URI}producto/listar`);
+    }
+
+    getOneProduct(){
+
+    }
+
+    saveProduct(){
+
+    }
+    
+    deleteProduct(){
+
+    }
+
+    updateProduct(){
+
+    }
+
+
+
+
 }
