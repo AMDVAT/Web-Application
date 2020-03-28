6dfc0ad8f7e4363b5f6824ab6537cf509b4ae0ce
Diego Josue Berrios Gutierrez <diegoberrios15@gmail.com>
2020-03-28 17:48:45 -0600

    update producto model
    se colocaron los atributos del modelo como opcionales

--- amdvatapp/src/app/models/Producto.ts
+++ amdvatapp/src/app/models/Producto.ts
@@ -1,12 +1,12 @@
 export class Producto {
-    id_producto: number;
-    nombre: string;
-    descripcion: string;
-    precio: number;
-    estado: number;
-    precio_oferta: number;
-    calificacion: number;
-    foto: string;
-    id_categoria: number;
-    nombre_categoria: string;
+    id_producto?: number;
+    nombre?: string;
+    descripcion?: string;
+    precio?: number;
+    estado?: number;
+    precio_oferta?: number;
+    calificacion?: number;
+    foto?: string;
+    id_categoria?: number;
+    nombre_categoria?: string;
 }
