cc9604b530c07772fa2165dd8da85f97bcd63d1d
Diego Josue Berrios Gutierrez <diegoberrios15@gmail.com>
2020-03-29 13:04:11 -0600

    create Sucursal Model
    se creo el modelo de datos de las sucursales

--- amdvatapp/src/app/models/sucursal.ts
+++ amdvatapp/src/app/models/sucursal.ts
@@ -1,12 +1,6 @@
 export class Sucursal {
-    id_producto?: number;
+    id?: number;
     nombre?: string;
-    descripcion?: string;
-    precio?: number;
-    estado?: number;
-    precio_oferta?: number;
-    calificacion?: number;
-    foto?: string;
-    id_categoria?: number;
-    nombre_categoria?: string;
+    direccion?: string;
+    numero?: number;
 }
