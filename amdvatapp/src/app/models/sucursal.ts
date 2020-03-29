a72104fb4b6e26d71c7216c29974c15698e8f268
Diego Josue Berrios Gutierrez <diegoberrios15@gmail.com>
2020-03-29 13:00:22 -0600

    create Sucursal service
    se creo el servicio para el CRUD de sucursales

---
+++ amdvatapp/src/app/models/sucursal.ts
@@ -0,0 +1,12 @@
+export class Sucursal {
+    id_producto?: number;
+    nombre?: string;
+    descripcion?: string;
+    precio?: number;
+    estado?: number;
+    precio_oferta?: number;
+    calificacion?: number;
+    foto?: string;
+    id_categoria?: number;
+    nombre_categoria?: string;
+}
