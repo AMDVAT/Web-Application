5f6ad88c2c31e859968457074208498b60d0200d
Diego Josue Berrios Gutierrez <diegoberrios15@gmail.com>
2020-03-30 18:04:05 -0600

    update Registro-Usuario
    actualmente ya se registra al usuario dentro de la plataforma.

--- amdvatapp/src/app/components/usuario-registro/usuario-registro.component.ts
+++ amdvatapp/src/app/components/usuario-registro/usuario-registro.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../../models/user';
+import {GestionUsuarioService} from '../../services/gestion-usuarios/gestion-usuario.service'
+import { AlertController } from '@ionic/angular';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-usuario-registro',
@@ -7,8 +11,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuarioRegistroComponent implements OnInit {
 
-  constructor() { }
+  usuario: User = {
+    nombre: '',
+    apellido: '',
+    email: '',
+    password: '',
+    tipo_usuario: 5
+}
+
+
+  constructor(private usuarioService: GestionUsuarioService, 
+    private alertController: AlertController,
+    private router: Router) { }
 
   ngOnInit() {}
 
+  saveUser(){
+    //Registrar Usuario
+    this.usuarioService.saveUser(this.usuario)
+    .subscribe( 
+      res =>{ 
+        this.messageSave();
+        //LOGEARLO AUTOMATICAMENTE
+
+        //GUARDAR EL TOKEN
+
+        //REDIRIGIR AL HOME
+        this.router.navigate(['home']); //<-----Esto ponerlo adentro del res del login 
+      }, 
+      err => {console.error(err); this.errorMessageSave();}
+      );
+    console.log(this.usuario);
+  }
+
+
+  async messageSave() {
+    const alert = await this.alertController.create({
+      header: 'Almacenado',
+      message: '<strong>Se ha registrado con exito. </strong>',
+      buttons: [
+        {
+          text: 'Aceptar',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  async errorMessageSave() {
+    const alert = await this.alertController.create({
+      header: 'Almacenado',
+      message: '<strong>No se ha logrado registrar,revise los datos </strong>',
+      buttons: [
+        {
+          text: 'Aceptar',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
 }
