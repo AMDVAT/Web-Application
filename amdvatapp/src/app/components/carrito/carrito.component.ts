1ca5a710129bc1318de1a2afbe95ceff87bcb544
Fernando Flores <nbafernandoflores8@gmail.com>
2020-03-30 20:32:17 -0600

    ci: carrito list update

--- amdvatapp/src/app/components/carrito/carrito.component.ts
+++ amdvatapp/src/app/components/carrito/carrito.component.ts
@@ -1,14 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Utils} from '../../Utils';
+import {Producto} from '../../models/Producto';
+import {CarritoService} from '../../services/carrito/carrito.service';
+import {ToastController} from '@ionic/angular';
 
 @Component({
-  selector: 'app-carrito',
-  templateUrl: './carrito.component.html',
-  styleUrls: ['./carrito.component.scss'],
+    selector: 'app-carrito',
+    templateUrl: './carrito.component.html',
+    styleUrls: ['./carrito.component.scss'],
 })
 export class CarritoComponent implements OnInit {
+    UtilsRef = Utils;
 
-  constructor() { }
+    constructor(
+        private carritoService: CarritoService,
+        private toastController: ToastController
+    ) {
+    }
 
-  ngOnInit() {}
+    ngOnInit() {
+    }
 
+    async presentToast() {
+        const toast = await this.toastController.create({
+            message: 'Producto añadido con exito!',
+            duration: 2000
+        });
+        toast.present();
+    }
+
+    AddProduct(producto: Producto) {
+      this.carritoService.UpdateCart(producto, 1);
+      this.presentToast();
+    }
 }
