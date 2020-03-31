da0ff08767b5664323d08b3aa53ec94b68853784
Fernando Flores <nbafernandoflores8@gmail.com>
2020-03-30 20:03:24 -0600

    ci(services/carrito): Update and Delete functions add

--- amdvatapp/src/app/services/carrito/carrito.service.ts
+++ amdvatapp/src/app/services/carrito/carrito.service.ts
@@ -1,22 +1,32 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Producto} from '../../models/Producto';
 import {Utils} from '../../Utils';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class CarritoService {
 
-  constructor() { }
+    constructor() {
+    }
 
-  public AddToCart(producto: Producto, amount: number): void{
-    const prod = {
-      producto,
-      amount
-    };
-    Utils.products.push(prod);
-    Utils.UpdateAmounts();
-  }
+    public UpdateCart(producto: Producto, amount: number): void {
+        const prod = {
+            producto,
+            amount
+        };
+        const index = Utils.products.findIndex((p) =>
+            p.producto.id_producto === producto.id_producto
+        );
+        if (index === -1) {
+            Utils.products.push(prod);
+        } else {
+            Utils.products[index].cantidad = amount;
+        }
+        Utils.UpdateAmounts();
+    }
 
-
+    public DeleteFromCart(producto: Producto): void {
+        Utils.products = Utils.products.filter((p) => p.producto.id_producto !== producto.id_producto);
+    }
 }
