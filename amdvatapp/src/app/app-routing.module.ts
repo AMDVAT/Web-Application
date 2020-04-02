b5c5e6ba3668a67cdfda7335e79000fc0f056050
Fernando Flores <nbafernandoflores8@gmail.com>
2020-04-02 00:51:08 -0600

    fix: routing auth validation add

--- amdvatapp/src/app/app-routing.module.ts
+++ amdvatapp/src/app/app-routing.module.ts
@@ -52,45 +52,45 @@ const routes: Routes = [
     },{
         path: 'gestion/usuario/lista',
         component: ListaUsuariosComponent,
-        //canActivate: [NeedAuthAdmin]
+        canActivate: [NeedAuthAdmin]
     },{
         path: 'gestion/usuario/lista/datos',
         component: UsuarioSimpleComponent,
-        //canActivate: [NeedAuthAdmin]
+        canActivate: [NeedAuthAdmin]
     },{
         path: 'gestion/usuario/perfil/:id',
         component: UsuarioPerfilComponent,
-        //canActivate: [NeedAuthAdmin]
+        canActivate: [NeedAuthAdmin]
     },{
         path: 'gestion/usuario/lista/editar/:id',
-        component: UsuarioSimpleComponent
-        //canActivate: [NeedAuthAdmin]
+        component: UsuarioSimpleComponent,
+        canActivate: [NeedAuthAdmin]
     },{
         path: 'gestion/producto/lista',
         component: ProductoListaComponent,
-        //canActivate: [NeedAuthAdmin]
+        canActivate: [NeedAuthAdmin]
     },{
         path: 'gestion/producto/lista/datos',
         //component: ProductoSimpleComponent,
-        component: PestanaMantenimientoComponent
-       //canActivate: [NeedAuthAdmin]
+        component: PestanaMantenimientoComponent,
+       canActivate: [NeedAuthAdmin]
     },{
         path: 'gestion/producto/lista/editar/:id',
         //component: ProductoSimpleComponent
-        component: PestanaMantenimientoComponent
-        //canActivate: [NeedAuthAdmin]
+        component: PestanaMantenimientoComponent,
+        canActivate: [NeedAuthAdmin]
     },{
         path: 'gestion/sucursal/lista',
         component: SucursalListaComponent,
-        //canActivate: [NeedAuthAdmin]
+        canActivate: [NeedAuthAdmin]
     },{
         path: 'gestion/sucursal/lista/datos',
         component: SucursalSimpleComponent,
-        //canActivate: [NeedAuthAdmin]
+        canActivate: [NeedAuthAdmin]
     },{
         path: 'gestion/sucursal/lista/editar/:id',
-        component: SucursalSimpleComponent
-        //canActivate: [NeedAuthAdmin]
+        component: SucursalSimpleComponent,
+        canActivate: [NeedAuthAdmin]
     },{
         path: 'productos/:producto',
         component: ProductosComponent,
@@ -104,19 +104,19 @@ const routes: Routes = [
     },{
         path: 'gestion/categoria/lista',
         component: CategoriaListaComponent,
-        // canActivate: [NeedAuthAdmin]
+         canActivate: [NeedAuthAdmin]
     },{
         path: 'gestion/categoria/lista/datos',
         component: CategoriaSimpleComponent,
-        // canActivate: [NeedAuthAdmin]
+         canActivate: [NeedAuthAdmin]
     },{
         path: 'gestion/categoria/lista/editar/:id',
         component: CategoriaSimpleComponent,
-        // canActivate: [NeedAuthAdmin]
+         canActivate: [NeedAuthAdmin]
     },{
         path: 'cart',
         component: CarritoComponent,
-        // canActivate: [NeedAuthAdmin]
+         canActivate: [NeedAuthAdmin]
     }
     ,{
         path: 'mantenimiento',
